0a23b3d8944101851f31af377de67e8aca739326
Elijah Ginter <ginterelijah@gmail.com>
2016-10-26 14:06:02 -0600

    js fixes

--- PerfectHome/WebContent/app_client/ngApp.js
+++ PerfectHome/WebContent/app_client/ngApp.js
@@ -4,20 +4,20 @@ var app = angular.module('ngApp', ['ngRoute'])
 .config(function($routeProvider){
   $routeProvider
     .when('/', {
-       templateUrl: 'public/views/templates/home.view.html',
-       controller: 'homeController'
+       templateUrl: 'public/views/welcome.view.html',
+       controller: 'welcomeController'
     })
     .when('/login', {
-      templateUrl: 'public/views/templates/login.view.html',
+      templateUrl: 'public/views/login.view.html',
       controller: 'loginController'
     })
     .when('/register', {
-      templateUrl: 'public/views/templates/register.view.html',
+      templateUrl: 'public/views/register.view.html',
       controller: 'registerController'
     })
-    .when('/todos', {
-      templateUrl: 'public/views/templates/todos.view.html',
-      controller: 'todosController'
+    .when('/user', {
+      templateUrl: 'public/views/user.view.html',
+      controller: 'userController'
     })
     .otherwise({
 
