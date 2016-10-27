0f5ff38a5d2891a230a96b3dad7bc496b9d7d946
Elijah Ginter <ginterelijah@gmail.com>
2016-10-27 10:52:56 -0600

    Registration tied to controller

--- PerfectHome/WebContent/app_client/controllers/registerController.js
+++ PerfectHome/WebContent/app_client/controllers/registerController.js
@@ -2,6 +2,11 @@
 
 var app = angular.module('ngApp');
 
-app.controller('registerController', function($scope){
+app.controller('registerController', function($scope, registrationService){
 	console.log('Register controller');
+	
+	$scope.makeUser = function(user){
+		console.log(user);
+		registrationService.createUser(user);
+	}
 });
