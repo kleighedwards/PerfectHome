c102ae3d8a838b0122e66f18cadabdb8f7bbb688
Rod Hammond <rodhammond@live.com>
2016-11-02 13:41:21 -0600

    latest ToDo updates

--- PerfectHome/WebContent/app_client/directives/todoRow/todoRow.js
+++ PerfectHome/WebContent/app_client/directives/todoRow/todoRow.js
@@ -6,7 +6,7 @@ app.directive('attrDirective', function($compile) {
     return {
         restrict: 'A',
         template: `
-                    <td id="td1">{{todo.task}}</td>
+                    <td style="text-align: left" id="td1">  {{todo.task}}</td>
         			<td id="td1">{{todo.date}}</td>
                     <td id="td1"><input type="checkbox" ng-model="todo.completed" ng-class="strike(todo.completed)" ng-change="edit(todo)"/></td>
                     <td id="td1">{{todo.completed}}</td>
@@ -37,20 +37,22 @@ app.directive('attrDirective', function($compile) {
                 if (editForm === null) {
                     $scope.todoCopy = angular.copy(todo);
                     var $inputRow =
-                        '<tr id="tr1">' +
-                        '<td id="td1">' +
-                        '<input type="text" class="form-control" ng-model="todoCopy.task" />' +
-                        '</td>' +
-                        '<td id="td1" colspan="3">' +
-                        '<input type="text" class="form-control" ng-model="todoCopy.date" />' +
-                        '</td>' +
-                        '<td id="td1">' +
-                        '<button ng-click="save(todoCopy)" class="btn btn-primary">Save</button>' +
-                        '</td>' +
-                        '<td id="td1">' +
-                        '<button ng-click="cancel()" class="btn btn-primary">Cancel</button>' +
-                        '</td>' +
-                        '</tr>'
+                      `
+                        <tr id="tr1">
+                        <td id="td1">
+                        <input type="text" class="form-control" ng-model="todoCopy.task" />
+                        </td>
+                        <td id="td1" colspan="3">
+                        <input type="date" class="form-control" ng-model="todoCopy.date" />
+                        </td>
+                        <td id="td1">
+                        <button ng-click="save(todoCopy)" class="btn btn-primary">Save</button>
+                        </td>
+                        <td id="td1">
+                        <button ng-click="cancel()" class="btn btn-primary">Cancel</button>
+                        </td>
+                        </tr>
+                      `
 
                     var compiledRow = $compile($inputRow)($scope);
                     editForm = compiledRow;
@@ -64,8 +66,7 @@ app.directive('attrDirective', function($compile) {
                     editForm = null;
                 }
             }
- 
-
+            
             $scope.save = function(todo,homeuserId) {
                 $scope.edit(todo,homeuserId); //todoService.updateTodo(todo);
                 editForm.remove();
